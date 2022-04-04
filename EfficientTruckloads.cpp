4efa5a65675fc292755559b7a0206bce0a46957d
domross02 <84361329+domross02@users.noreply.github.com>
2022-04-04 14:56:42 +0930

    Add files via upload

---
+++ EfficientTruckloads.cpp
@@ -0,0 +1,33 @@
+#include "EfficientTruckloads.h"
+
+#include <math.h>
+
+
+int EfficientTruckloads::numTrucks(int numCrates, int loadSize){
+if(numCrates<=loadSize||loadSize<=0||numCrates<=0){
+    return 1;
+}
+if(!crateslookupTable.empty()){
+    for(int i = 0; i<crateslookupTable.capacity();i++){
+        if(numCrates==crateslookupTable.at(i)){
+            return reslookupTable.at(i);
+        }
+    }
+
+}
+    int pile1;
+    int pile2;
+
+    if(numCrates % 2 == 0){
+        pile1=numCrates/2;
+        pile2=numCrates/2;
+    }
+    else{
+        pile1=numCrates/2;
+        pile2=(numCrates/2)+1;
+    }
+    int num = numTrucks(pile1, loadSize)+numTrucks(pile2, loadSize);
+    crateslookupTable.push_back(numCrates);
+    reslookupTable.push_back(num);
+    return num;
+}
---
+++ EfficientTruckloads.h
@@ -0,0 +1,10 @@
+#ifndef EFFICIENTTRUCKLOADS
+#define EFFICIENTTRUCKLOADS
+#include <vector>
+class EfficientTruckloads{
+public:
+    int numTrucks(int, int);
+private:
+std::vector<int> reslookupTable, crateslookupTable;
+};
+#endif
